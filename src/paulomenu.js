f7dc171a3fd9594c14a1bba1884d0b15a839a6eb
japawar <japawar30@gmail.com>
2021-03-02 00:38:35 -0400

    First commit

---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const japamenu = (prefix, pushname) => {
+    return `◪ *Comandos do japa Ofc*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = japamenu
